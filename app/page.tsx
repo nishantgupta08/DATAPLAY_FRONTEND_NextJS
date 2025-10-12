6995902f751e019e0e4758e5ebdcb97268970fcd
Nishant Gupta <nishantthewriter@gmail.com>
2025-10-13 03:54:04 +0530

    fixed bug

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,6 @@ export default function Home() {
       <Features />
       <FellowshipPrograms />
       <Courses />
-      <PrimaryCTASection />
       <Mentors />
       <BecomeMentorInlineForm />
       <StickyCTA />
