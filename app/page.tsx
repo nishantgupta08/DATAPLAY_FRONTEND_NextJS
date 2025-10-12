80bbc8c8d4b5cf0b2af535a1898b1867bf7f6e69
nishantgupta08 <nishantthewriter@gmail.com>
2025-10-13 03:30:43 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,10 @@ export default function Home() {
       <Features />
       <FellowshipPrograms />
       <Courses />
+      <PrimaryCTASection />
       <Mentors />
+      <BecomeMentorInlineForm />
+      <StickyCTA />
       <Testimonials />
       <WhoCanApply />
       <Gallery />
